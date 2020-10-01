25aab3402bd54c9c86f359c28991bf871f7cccd3
Rocio Alvarez Bernal <dev.alvarezbernal@gmail.com>
2020-10-01 12:17:05 -0500

    added pseudo and imports

---
+++ drawLines.java
@@ -0,0 +1,13 @@
+//import java.awt.event.MouseEvent;
+import java.awt.event.*;
+import acm.graphics.*;
+import acm.program.*;
+//import acm.program.GraphicsProgram;
+
+public class drawLines extends GraphicsProgram{
+	public void run() {
+		
+	}
+	
+}
+
