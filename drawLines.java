9f1e5cdb7914ea8ee472bc37605de4d285acd135
Rocio Alvarez Bernal <dev.alvarezbernal@gmail.com>
2020-10-01 12:44:52 -0500

    added methods

--- drawLines.java
+++ drawLines.java
@@ -1,13 +1,36 @@
+//Psuedo code 
+//listen for click get start coordinate draw a line to end click coordinates
+
 //import java.awt.event.MouseEvent;
 import java.awt.event.*;
 import acm.graphics.*;
 import acm.program.*;
 //import acm.program.GraphicsProgram;
 
-public class drawLines extends GraphicsProgram{
+public class drawLines extends GraphicsProgram {
 	public void run() {
+		draw(){
+			startPoint();
+			endPoint();
+		}
+		
+	private void startPoint(mouse) {
+		getCoordinates()
+		
+	}
+	private void endPoint(mouse) {
+		getCoordinates()
 		
 	}
 	
-}
+	private void getCoordinates() {
+		double xCoordinate = ;
+		double yCoordinate = ;
+		double xy = (xCoordinate, yCoordinate)
+	}
 
+
+	
+	}
+
+}
